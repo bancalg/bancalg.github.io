7eb6e04d832a61650438004f0aa72af387d8c816
Jean-Daniel Bancal <jdbancal@gmail.com>
2025-07-23 22:13:48 +0200

    first version

---
+++ header.php
@@ -0,0 +1,34 @@
+<!-- See OpenClassroom :
+https://openclassrooms.com/en/courses/918836-concevez-votre-site-web-avec-php-et-mysql/4239271-organisez-les-pages-de-votre-site-en-blocs-fonctionnels
+ -->
+
+ <html xmlns="https://www.w3.org/1999/xhtml" xml:lang="en" lang="en-us">
+
+<head>
+    <link href="https://gmpg.org/xfn/11" rel="profile">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+    <!-- Enable responsiveness on mobile devices-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+
+    <title>
+        Pierre's webpage &middot; PhD Student
+    </title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="public/css/poole.css">
+    <link rel="stylesheet" href="public/css/syntax.css">
+    <link rel="stylesheet" href="public/css/hyde.css">
+    <link rel="stylesheet" href="public/css/font.css">
+
+    <link rel="shortcut icon" type="image/x-icon" href="Images/PR-box-square.ico">
+
+    <meta name="twitter:card" content="summary"></meta>
+    <meta name="twitter:title" content="Pierre's webpage"></meta>
+    <meta name="twitter:image" content="Images/PR-box-large.png"></meta>
+    <meta name="twitter:image:src" content="Images/PR-box-large.png"></meta>
+    <meta name="twitter:description" content='I am a PhD student in Quantum Information Theory.'></meta>
+
+    <script src="public/js/font.js" crossorigin="anonymous"></script>
+</head>
